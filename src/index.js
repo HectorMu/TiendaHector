d497160b3c610129e3b1f085c945a963b652cf7c
Héctor Manuel Muñoz Flores <skyspartanblack@gmail.com>
2021-10-10 02:32:49 -0500

    little changes

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ let options ={
     host: process.env.DB_HOST,
     port: 3306,
     user: process.env.DB_USER,
-    password:'',
+    password:process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
 };
 
